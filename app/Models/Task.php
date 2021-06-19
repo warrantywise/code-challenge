8031a7547f67ae8c83b9594e45875005a3accdcd
LewisWH <72448549+LewisWH@users.noreply.github.com>
2021-06-19 22:55:05 +0100

    Fixed version and routing issues
    Updated packages
    Moved models into folder
    Corrected namespaces
    Initialise database

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    use HasFactory;
 
     protected $fillable = ['title','description', 'due_date', 'due_time'];
     
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 // });
 
-// Route::get('/', 'AppController@show');
-
 Route::get('{any}', function () {
    return view('app');
 })->where('any', '.*');
